eb52760982f45c8d8c5785c8b54444c7d9512bd9
James Gunter <jpgunter@gmail.com>
2011-03-04 14:34:59 -0800

    now the character is a guy and not a plane

---
+++ src/com/uwtgpc/shadowgames/util/Direction.java
@@ -0,0 +1,8 @@
+package com.uwtgpc.shadowgames.util;
+
+/**
+ * Created by IntelliJ IDEA. User: jim Date: 3/4/11 Time: 2:14 PM To change this template use File | Settings | File
+ * Templates.
+ */
+public enum Direction{
+}
